af2723c460124be2bd2021c3e6bae4cf07e70794
Devrath <devrath.dev595@gmail.com>
2023-10-29 19:34:18 +0530

    revert

--- Code/CurrencyConversionApp/app/src/main/java/com/istudio/code/MainActivity.kt
+++ Code/CurrencyConversionApp/app/src/main/java/com/istudio/code/MainActivity.kt
@@ -13,20 +13,17 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -61,110 +58,75 @@ class MainActivity : ComponentActivity() {
 
             MaterialAppTheme(darkTheme = darkTheme) {
                 // A surface container using the 'background' color from the theme
-                RootComposable(
-                    snackBarController = snackBarController,
-                    scrollBehaviour = scrollBehaviour,
-                    titleStr = titleStr,
-                    darkTheme = darkTheme,
-                    controller = controller,
-                    focusManager = focusManager
-                )
+
+                Scaffold(
+                    snackbarHost = { SnackbarHost(snackBarController) },
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .nestedScroll(scrollBehaviour.nestedScrollConnection),
+                    topBar = {
+
+                        TopAppBar(
+                            title = { Text(text = titleStr) },
+                            scrollBehavior = scrollBehaviour,
+                            actions = {
+                                ThemeSwitcher(
+                                    darkTheme = darkTheme,
+                                    size = 50.dp,
+                                    padding = 5.dp,
+                                    onClick = {
+                                        // Update theme on-click
+                                        darkTheme = !darkTheme
+                                    }
+                                )
+                            }
+                        )
+                    }
+                ) {
+                    Box(
+                        modifier = Modifier.padding(it)
+                    ) {
+                        NavHost(
+                            navController = controller,
+                            startDestination = Route.CURRENCY_CONVERSION_SCREEN
+                        ) {
+
+                            composable(route = Route.CURRENCY_CONVERSION_SCREEN) {
+                                CurrencyScreen(
+                                    onErrorAction = { message ->
+                                        // Scenario : When error occurs
+
+                                    },
+                                    onKeyBoardOutsideClick = {
+                                        // Scenario : When user clicks outside the keyboard
+                                        focusManager.clearFocus()
+                                    },
+                                    onBackPress = {
+                                        // Scenario : When user presses back button
+
+                                    }
+                                )
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 }
 
 @Composable
-@OptIn(ExperimentalMaterial3Api::class)
-fun RootComposable(
-    snackBarController: SnackbarHostState,
-    scrollBehaviour: TopAppBarScrollBehavior,
-    titleStr: String,
-    darkTheme: Boolean,
-    controller: NavHostController,
-    focusManager: FocusManager
-) {
-    var darkTheme1 = darkTheme
-    Scaffold(
-        snackbarHost = { SnackbarHost(snackBarController) },
-        modifier = Modifier
-            .fillMaxSize()
-            .nestedScroll(scrollBehaviour.nestedScrollConnection),
-        topBar = {
-
-            TopAppBar(
-                title = { Text(text = titleStr) },
-                scrollBehavior = scrollBehaviour,
-                actions = {
-                    ThemeSwitcher(
-                        darkTheme = darkTheme1,
-                        size = 50.dp,
-                        padding = 5.dp,
-                        onClick = {
-                            // Update theme on-click
-                            darkTheme1 = !darkTheme1
-                        }
-                    )
-                }
-            )
-        }
-    ) {
-        Box(
-            modifier = Modifier.padding(it)
-        ) {
-            NavHost(
-                navController = controller,
-                startDestination = Route.CURRENCY_CONVERSION_SCREEN
-            ) {
-
-                composable(route = Route.CURRENCY_CONVERSION_SCREEN) {
-                    CurrencyScreen(
-                        onErrorAction = { message ->
-                            // Scenario : When error occurs
-
-                        },
-                        onKeyBoardOutsideClick = {
-                            // Scenario : When user clicks outside the keyboard
-                            focusManager.clearFocus()
-                        },
-                        onBackPress = {
-                            // Scenario : When user presses back button
-
-                        }
-                    )
-                }
-            }
-        }
-    }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    var darkTheme by remember { mutableStateOf(false) }
-    // Title
-    val titleStr = "Currency Converter"
-    // <!--------------------- CONTROLLERS ------------------------>
-    // SnackBar controller
-    val snackBarController = remember { SnackbarHostState() }
-    // Scroll behaviour
-    val scrollBehaviour = TopAppBarDefaults.pinnedScrollBehavior()
-    // Nav controller
-    val controller = rememberNavController()
-    // Keyboard controller
-    val keyboardController = LocalSoftwareKeyboardController.current
-    // Focus Manager
-    val focusManager = LocalFocusManager.current
-    // <!--------------------- CONTROLLERS ------------------------>
     MaterialAppTheme {
-        RootComposable(
-            snackBarController = snackBarController,
-            scrollBehaviour = scrollBehaviour,
-            titleStr = titleStr,
-            darkTheme = darkTheme,
-            controller = controller,
-            focusManager = focusManager
-        )
+        Greeting("Android")
     }
 }
