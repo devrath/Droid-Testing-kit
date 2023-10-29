dcb0755607de7b402e2e56d3e8551a828cd75363
Devrath <devrath.dev595@gmail.com>
2023-10-29 10:33:16 +0530

    refractor

--- Code/CurrencyConversionApp/app/build.gradle.kts
+++ Code/CurrencyConversionApp/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_18
     }
     kotlinOptions {
-        jvmTarget = "18"
+        jvmTarget = ProjectConfig.jvmTarget
     }
     buildFeatures {
         compose = true
@@ -52,21 +52,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     appModuleDependencies()
     implementation(Dependencies.hiltNavigationCompose)
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
     //--> Module link to currency Feature
     featureCurrencyConverter()
     coreUi()
-
 }
