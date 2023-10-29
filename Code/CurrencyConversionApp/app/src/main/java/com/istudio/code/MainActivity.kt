db49bf1548468fbbb9d0a2d4796af23ee51b2b66
Devrath <devrath.dev595@gmail.com>
2023-10-29 18:44:27 +0530

    state hoisting

--- Code/CurrencyConversionApp/app/src/main/java/com/istudio/code/MainActivity.kt
+++ Code/CurrencyConversionApp/app/src/main/java/com/istudio/code/MainActivity.kt
@@ -3,52 +3,112 @@ package com.istudio.code
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.istudio.core.navigation.Route
+import com.istudio.core_ui.composables.ThemeSwitcher
 import com.istudio.core_ui.theme.MaterialAppTheme
 import com.istudio.currency_converter.presentation.CurrencyScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // Theme switcher
             var darkTheme by remember { mutableStateOf(false) }
+            // Title
+            val titleStr = "Currency Converter"
+            // <!--------------------- CONTROLLERS ------------------------>
+            // SnackBar controller
+            val snackBarController = remember { SnackbarHostState() }
+            // Scroll behaviour
+            val scrollBehaviour = TopAppBarDefaults.pinnedScrollBehavior()
+            // Nav controller
+            val controller = rememberNavController()
+            // Keyboard controller
+            val keyboardController = LocalSoftwareKeyboardController.current
+            // Focus Manager
+            val focusManager = LocalFocusManager.current
+            // <!--------------------- CONTROLLERS ------------------------>
+            
             MaterialAppTheme(darkTheme = darkTheme) {
                 // A surface container using the 'background' color from the theme
 
-                val controller = rememberNavController()
+                Scaffold(
+                    snackbarHost = { SnackbarHost(snackBarController) },
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .nestedScroll(scrollBehaviour.nestedScrollConnection),
+                    topBar = {
 
-                NavHost(
-                    navController = controller,
-                    startDestination = Route.CURRENCY_CONVERSION_SCREEN
-                ) {
-
-                    composable(route = Route.CURRENCY_CONVERSION_SCREEN){
-                        CurrencyScreen(
-                            darkTheme = darkTheme,
-                            onThemeUpdated = {
-                                darkTheme = !darkTheme
+                        TopAppBar(
+                            title = { Text(text = titleStr) },
+                            scrollBehavior = scrollBehaviour,
+                            actions = {
+                                ThemeSwitcher(
+                                    darkTheme = darkTheme,
+                                    size = 50.dp,
+                                    padding = 5.dp,
+                                    onClick = {
+                                        // Update theme on-click
+                                        darkTheme = !darkTheme
+                                    }
+                                )
                             }
-                        ){
+                        )
+                    }
+                ) {
+                    Box(
+                        modifier = Modifier.padding(it)
+                    ) {
+                        NavHost(
+                            navController = controller,
+                            startDestination = Route.CURRENCY_CONVERSION_SCREEN
+                        ) {
 
+                            composable(route = Route.CURRENCY_CONVERSION_SCREEN) {
+                                CurrencyScreen(
+                                    onErrorAction = { message ->
+                                        // Scenario : When error occurs
+
+                                    },
+                                    onKeyBoardOutsideClick = {
+                                        // Scenario : When user clicks outside the keyboard
+                                        focusManager.clearFocus()
+                                    },
+                                    onBackPress = {
+                                        // Scenario : When user presses back button
+
+                                    }
+                                )
+                            }
                         }
                     }
-
                 }
             }
         }
--- Code/CurrencyConversionApp/features/currency-converter/src/main/java/com/istudio/currency_converter/presentation/CurrencyScreen.kt
+++ Code/CurrencyConversionApp/features/currency-converter/src/main/java/com/istudio/currency_converter/presentation/CurrencyScreen.kt
@@ -10,44 +10,31 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.istudio.core_ui.composables.DropDownField
 import com.istudio.core_ui.composables.GridInput
 import com.istudio.core_ui.composables.InputTextField
-import com.istudio.core_ui.composables.ThemeSwitcher
 import com.istudio.core_ui.theme.LocalSpacing
-import com.istudio.currency_converter.R
 import com.istudio.currency_converter.presentation.states.CurrencyScreenResponseEvent
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun CurrencyScreen(
     modifier: Modifier = Modifier,
-    darkTheme: Boolean,
-    onThemeUpdated: () -> Unit,
+    onErrorAction: (String) -> Unit,
+    onKeyBoardOutsideClick: () -> Unit,
     onBackPress: () -> Unit
 ) {
 
@@ -59,25 +46,6 @@ fun CurrencyScreen(
     // View state reference from view model
     val state = viewModel.viewState
     // <!----------- MAIN-COMPOSE-CONTROL-PARTS ------------------->
-
-    // <!--------------------- CONTROLLERS ------------------------>
-    // Keyboard controller
-    val keyboardController = LocalSoftwareKeyboardController.current
-    // Focus Manager
-    val focusManager = LocalFocusManager.current
-    // SnackBar controller
-    val snackBarController = remember { SnackbarHostState() }
-    // Coroutine to handle the animation
-    val coroutineScopeController = rememberCoroutineScope()
-    // <!--------------------- CONTROLLERS ------------------------>
-
-    // Scroll behaviour
-    val scrollBehaviour = TopAppBarDefaults.pinnedScrollBehavior()
-
-
-    //val titleStr = cxt.getString(R.string.str_currency_converter)
-    val titleStr = "Currency Converter"
-
 
     LaunchedEffect(key1 = state.launchedEffectState) {
 
@@ -94,76 +62,50 @@ fun CurrencyScreen(
             }
         }
         // <***********> Event is observed from View-Model <************>
-
     }
 
-
-    Scaffold(
-        snackbarHost = { SnackbarHost(snackBarController) },
-        modifier = Modifier.fillMaxSize()
-            .nestedScroll(scrollBehaviour.nestedScrollConnection),
-        topBar = {
-
-            TopAppBar(
-                title = { Text(text = titleStr) },
-                scrollBehavior = scrollBehaviour,
-                actions = {
-                    ThemeSwitcher(
-                        darkTheme = darkTheme,
-                        size = 50.dp,
-                        padding = 5.dp,
-                        onClick = onThemeUpdated
-                    )
-                }
-            )
-        }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(LocalSpacing.current.spaceExtraSmall)
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    onKeyBoardOutsideClick.invoke()
+                })
+            },
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Center
     ) {
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(it)
-                .padding(LocalSpacing.current.spaceExtraSmall)
-                .pointerInput(Unit){
-                    detectTapGestures(onTap = {
-                        focusManager.clearFocus()
-                    })
-                },
-            horizontalAlignment = Alignment.End,
-            verticalArrangement = Arrangement.Center
+        InputTextField(modifier = Modifier.fillMaxWidth())
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
         ) {
 
-            InputTextField(modifier = Modifier.fillMaxWidth())
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-
-                Text(
-                    text = "Output",
-                    style = MaterialTheme.typography.titleMedium,
-                    modifier = Modifier
-                        .weight(1.8f)
-                        .padding(
-                            horizontal = LocalSpacing.current.spaceExtraSmall,
-                            vertical = 20.dp
-                        ),
-                    textAlign = TextAlign.Start,
-                    maxLines = 1
-                )
-
-                Spacer(modifier = Modifier.height(LocalSpacing.current.spaceExtraSmall))
-
-                Box(modifier = Modifier.weight(1f)) { DropDownField() }
-            }
+            Text(
+                text = "Output",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier
+                    .weight(1.8f)
+                    .padding(
+                        horizontal = LocalSpacing.current.spaceExtraSmall,
+                        vertical = 20.dp
+                    ),
+                textAlign = TextAlign.Start,
+                maxLines = 1
+            )
 
             Spacer(modifier = Modifier.height(LocalSpacing.current.spaceExtraSmall))
 
-            GridInput()
+            Box(modifier = Modifier.weight(1f)) { DropDownField() }
         }
-    }
 
+        Spacer(modifier = Modifier.height(LocalSpacing.current.spaceExtraSmall))
+
+        GridInput()
+    }
 
 }
