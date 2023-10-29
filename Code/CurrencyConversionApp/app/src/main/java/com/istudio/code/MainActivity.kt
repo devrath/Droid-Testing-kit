85473221fbf127e5c22d982e559b024abb93f9be
Devrath <devrath.dev595@gmail.com>
2023-10-29 10:58:44 +0530

    entry point added

--- Code/CurrencyConversionApp/app/src/main/java/com/istudio/code/MainActivity.kt
+++ Code/CurrencyConversionApp/app/src/main/java/com/istudio/code/MainActivity.kt
@@ -11,7 +11,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.istudio.core_ui.theme.MaterialAppTheme
+import com.istudio.currency_converter.presentation.CurrencyScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +25,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CurrencyScreen(){
+
+                    }
                 }
             }
         }
