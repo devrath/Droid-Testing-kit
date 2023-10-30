d869ab9795883aa0560d997e154024d695c61b48
Devrath <devrath.dev595@gmail.com>
2023-10-30 20:15:53 +0530

    removed code

--- Code/Applications/ivy-wallet-main/ivy-data/src/main/java/com/ivy/data/repository/impl/TransactionRepositoryImpl.kt
+++
@@ -1,7 +0,0 @@
-package com.ivy.data.repository.impl
-
-import javax.inject.Inject
-
-class TransactionRepositoryImpl @Inject constructor() {
-    // TODO: Implement
-}
--- Code/Applications/ivy-wallet-main/screen-piechart/src/main/java/com/ivy/piechart/SelectedCategory.kt
+++
@@ -1,9 +0,0 @@
-package com.ivy.piechart
-
-import androidx.compose.runtime.Immutable
-import com.ivy.legacy.datamodel.Category
-
-@Immutable
-data class SelectedCategory(
-    val category: Category? // null - Unspecified
-)
