de0ceefe0eacc71fd3c7a8faec42701b6339642c
Devrath <devrath.dev595@gmail.com>
2023-10-30 10:21:48 +0530

    Refactoring

--- Code/CurrencyConversionApp/features/common-feature/src/main/java/com/istudio/common_feature/domain/usecases/useCaseMain/CheckConnectivityUseCase.kt
+++ Code/CurrencyConversionApp/features/common-feature/src/main/java/com/istudio/common_feature/domain/usecases/useCaseMain/CheckConnectivityUseCase.kt
@@ -2,6 +2,7 @@ package com.istudio.common_feature.domain.usecases.useCaseMain
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.Network
 import android.net.NetworkCapabilities
 import android.os.Build
 import com.istudio.core.platform.actions.FlowAction
@@ -11,30 +12,63 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
+@Suppress("DEPRECATION")
 class CheckConnectivityUseCase @Inject constructor(
     private val connectivityManager: ConnectivityManager
 ) : FlowAction<Unit, Boolean>() {
 
     override fun createFlow(input: Unit): Flow<Boolean> {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val network = connectivityManager.activeNetwork ?: return flowOf(false)
-            val activeNetwork =
-                connectivityManager.getNetworkCapabilities(network) ?: return flowOf(false)
+        var isConnectedToInternet = false
 
-            return when {
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> flowOf(true)
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> flowOf(true)
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> flowOf(true)
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> flowOf(true)
-                else -> flowOf(false)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(connectivityManager.activeNetwork!=null){
+                val network = connectivityManager.activeNetwork
+                isConnectedToInternet = checkNetworkCases(network, isConnectedToInternet)
+            }else{
+                isConnectedToInternet = false
             }
         } else {
-            @Suppress("DEPRECATION") val networkInfo =
-                connectivityManager.activeNetworkInfo ?: return flowOf(false)
-            @Suppress("DEPRECATION")
-            return flowOf(networkInfo.isConnected)
+            if(connectivityManager.activeNetworkInfo!=null){
+                val networkInfo = connectivityManager.activeNetworkInfo
+                isConnectedToInternet = networkInfo?.isConnected ?: false
+            }else{
+                isConnectedToInternet = false
+            }
         }
+        return flowOf(isConnectedToInternet)
+    }
+
+    private fun checkNetworkCases(
+        network: Network?,
+        isConnectedToInternet: Boolean
+    ): Boolean {
+        var isConnectedToInternet1 = isConnectedToInternet
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network)
+        if (activeNetwork != null) {
+            when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                    isConnectedToInternet1 = true
+                }
+
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                    isConnectedToInternet1 = true
+                }
+
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                    isConnectedToInternet1 = true
+                }
+
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> {
+                    isConnectedToInternet1 = true
+                }
+
+                else -> {
+                    isConnectedToInternet1 = false
+                }
+            }
+        }
+        return isConnectedToInternet1
     }
 
 }
