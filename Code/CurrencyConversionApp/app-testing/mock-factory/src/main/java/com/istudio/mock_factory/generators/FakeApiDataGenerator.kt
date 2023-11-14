7787f4da1174cfc5915adc7d8d8d3c6a6ce5dbee
Devrath <devrath.dev595@gmail.com>
2023-11-14 10:26:54 +0530

    more mock changes

--- Code/CurrencyConversionApp/app-testing/mock-factory/src/main/java/com/istudio/mock_factory/generators/FakeApiDataGenerator.kt
+++ Code/CurrencyConversionApp/app-testing/mock-factory/src/main/java/com/istudio/mock_factory/generators/FakeApiDataGenerator.kt
@@ -1,12 +1,20 @@
 package com.istudio.mock_factory.generators
 
 import com.google.gson.Gson
+import com.istudio.mock_factory.generators.FakeApiData.ServerResources.CURRENCIES_FILE
+import com.istudio.mock_factory.generators.FakeApiData.ServerResources.CURRENCY_CONVERSION_VALUES_FILE
 import com.istudio.models.custom.MasterApiData
 import com.istudio.models.remote.Currencies
 import com.istudio.models.remote.CurrencyConversionValues
 import com.istudio.utils.FileUtil
 
 object FakeApiData {
+
+    object ServerResources {
+        const val CURRENCIES_FILE = "Currencies.json"
+        const val CURRENCY_CONVERSION_VALUES_FILE = "CurrencyConversionValues.json"
+    }
+
 
     fun getMasterApiData(
         currencies :Currencies, currencyConversionValues : CurrencyConversionValues
@@ -15,12 +23,12 @@ object FakeApiData {
     }
 
     fun getFakeApiCurriencies() : Currencies {
-        val data = FileUtil.kotlinReadFileWithNewLineFromResources("Currencies.json")
+        val data = FileUtil.kotlinReadFileWithNewLineFromResources(CURRENCIES_FILE)
         return Gson().fromJson(data, Currencies::class.java)
     }
 
     fun getFakeApiCurriencyConversionValues() : CurrencyConversionValues {
-        val data = FileUtil.kotlinReadFileWithNewLineFromResources("CurrencyConversionValues.json")
+        val data = FileUtil.kotlinReadFileWithNewLineFromResources(CURRENCY_CONVERSION_VALUES_FILE)
         return Gson().fromJson(data, CurrencyConversionValues::class.java)
     }
 
